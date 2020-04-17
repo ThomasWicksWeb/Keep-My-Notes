821dcfcd5f5e7ec19603289b3a1c89eb2f314091
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-04-17 13:04:28 -0400

    Removed unnecessary comments

--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import * as firebase from 'firebase'
-
-// // Your web app's Firebase configuration
-// var firebaseConfig = {
-//     apiKey: "AIzaSyAuu4t_3W5SLjCoQAnvxCbPrJIjL-EYw8w",
-//     authDomain: "keepmynotes-f9ade.firebaseapp.com",
-//     databaseURL: "https://keepmynotes-f9ade.firebaseio.com",
-//     projectId: "keepmynotes-f9ade",
-//     storageBucket: "keepmynotes-f9ade.appspot.com",
-//     messagingSenderId: "550390801016",
-//     appId: "1:550390801016:web:4f55f59dc7d5060da6438d",
-//     measurementId: "G-QC7YZL9R8X"
-// };
-
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-// firebase.analytics();
-
 
 ReactDOM.render(
   <React.StrictMode>
