67000ce8fe06ae53a4c8ee7189cfde123b714106
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-04-20 17:37:12 -0400

    Ran Prettier

--- src/components/StandardLogin/index.jsx
+++ src/components/StandardLogin/index.jsx
@@ -1,81 +1,99 @@
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 
-import firebase from 'firebase'
+import firebase from 'firebase';
 
-import 'bulma/css/bulma.css'
-import styles from './Login.module.scss'
+import 'bulma/css/bulma.css';
+import styles from './Login.module.scss';
 
 const StandardLogin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const history = useHistory();
 
-    const history = useHistory();
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(function (user) {
+        // user signed in
+        history.push('/home');
+      })
+      .catch(function (error) {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(`Error Code: ${errorCode}`);
+        console.log(`Error Message: ${errorMessage}`);
+        // ...
+      });
+  };
 
-    const handleOnSubmit = (e) => {
-        e.preventDefault();
-        firebase.auth().signInWithEmailAndPassword(email, password).then(function(user) {
-            // user signed in
-            history.push("/home");
-         }).catch(function(error) {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(`Error Code: ${errorCode}`);
-            console.log(`Error Message: ${errorMessage}`);
-            // ...
-          });
-    };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
 
-    const handleEmail = (e) => {
-        setEmail(e.target.value);
-    }
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
 
-    const handlePassword = e => {
-        setPassword(e.target.value);
-    }
+  return (
+    <section className="section">
+      <div className="container">
+        <h3 className="has-text-weight-bold has-text-centered is-size-3">
+          Login
+        </h3>
+        <h3 className="has-text-weight-bold has-text-centered is-size-4">
+          with Username or Email
+        </h3>
 
-
-    return(
-        <section className="section">
-            <div className="container">
-
-                <h3 className="has-text-weight-bold has-text-centered is-size-3">Login</h3>
-                <h3 className="has-text-weight-bold has-text-centered is-size-4">with Username or Email</h3>
-
-                <form onSubmit={handleOnSubmit} className={styles.formContainer}>
-                    <div className="field">
-                        <label className="label">Email</label>
-                        <div className="control">
-                            <input className="input" type="email" placeholder="Email" required value={email} onChange={handleEmail} />
-                        </div>
-                    </div>
-                    <div className="field">
-                        <label className="label">Password</label>
-                        <div className="control">
-                            <input className="input" type="password" placeholder="Password" required  value={password} onChange={handlePassword} />
-                        </div>
-                    </div>
-                    <button className="button is-info has-text-weight-bold" type="submit">Login</button>
-
-                    <hr />
-
-                    <p className="has-text-centered">Don't have an account?{' '}
-                        <Link to="/createaccount">Create one</Link>
-                    </p>
-
-                    <p className="has-text-centered">Having trouble logging in?{' '}
-                        <Link to="/resetpassword">Reset your password</Link>
-                    </p>
-
-                </form>
-
+        <form onSubmit={handleOnSubmit} className={styles.formContainer}>
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control">
+              <input
+                className="input"
+                type="email"
+                placeholder="Email"
+                required
+                value={email}
+                onChange={handleEmail}
+              />
             </div>
-        </section>
-    )
+          </div>
+          <div className="field">
+            <label className="label">Password</label>
+            <div className="control">
+              <input
+                className="input"
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                onChange={handlePassword}
+              />
+            </div>
+          </div>
+          <button className="button is-info has-text-weight-bold" type="submit">
+            Login
+          </button>
 
+          <hr />
 
-}
+          <p className="has-text-centered">
+            Don't have an account? <Link to="/createaccount">Create one</Link>
+          </p>
+
+          <p className="has-text-centered">
+            Having trouble logging in?{' '}
+            <Link to="/resetpassword">Reset your password</Link>
+          </p>
+        </form>
+      </div>
+    </section>
+  );
+};
 
 export default StandardLogin;
