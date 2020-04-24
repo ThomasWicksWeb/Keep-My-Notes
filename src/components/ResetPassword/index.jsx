a7c456fdbf2a0c84349273e8bb074be0d79a260d
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-04-24 19:53:01 -0400

    Added comments for code on how to reset password

--- src/components/ResetPassword/index.jsx
+++ src/components/ResetPassword/index.jsx
@@ -1,53 +1,70 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import 'firebase'
+import {auth} from '../../firebase'
 
-import 'bulma/css/bulma.css'
-import styles from './ResetPassword.module.scss'
+import 'bulma/css/bulma.css';
+import styles from './ResetPassword.module.scss';
 
 const ResetPassword = () => {
-    
-    const [emailField, setEmail] = useState("");
+  const [emailField, setEmail] = useState('');
 
-    const handleEmail = (e) => {
-        setEmail(e.target.value);
-    }
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
 
-    const handleOnSubmit = e => {
-        e.preventDefault();
-    }
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+  };
 
+//   var emailAddress = 'stringemail';
 
+//   auth
+//     .sendPasswordResetEmail(emailAddress)
+//     .then(function () {
+//       // Email sent.
+//     })
+//     .catch(function (error) {
+//       // An error happened.
+//     });
 
-    return(
-        <section className="section">
-            <div className="container">
+  return (
+    <section className="section">
+      <div className="container">
+        <h3 className="has-text-weight-bold has-text-centered is-size-3">
+          Reset your Password
+        </h3>
 
-                <h3 className="has-text-weight-bold has-text-centered is-size-3">Reset your Password</h3>
-
-                <form onSubmit={handleOnSubmit} className={styles.formContainer}>
-
-                    <div className="field">
-                        <label className="label">Email</label>
-                        <div className="control">
-                            <input className="input" type="email" placeholder="Enter your Email" required value={emailField} onChange={handleEmail} />
-                        </div>
-                    </div>
-
-                    <button className="button is-info has-text-weight-bold" type="submit">Reset Password</button>
-
-                    <p className="has-text-centered">Rememered your password?{' '}
-                        <Link to="/login">Login</Link>
-                    </p>
-
-                    <p className="has-text-centered">Don't have an account?{' '}
-                        <Link to="/createaccount">Create one</Link>
-                    </p>
-                    
-                </form>
-
+        <form onSubmit={handleOnSubmit} className={styles.formContainer}>
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control">
+              <input
+                className="input"
+                type="email"
+                placeholder="Enter your Email"
+                required
+                value={emailField}
+                onChange={handleEmail}
+              />
             </div>
-        </section>
-    )
-}
+          </div>
 
-export default ResetPassword
+          <button className="button is-info has-text-weight-bold" type="submit">
+            Reset Password
+          </button>
+
+          <p className="has-text-centered">
+            Rememered your password? <Link to="/login">Login</Link>
+          </p>
+
+          <p className="has-text-centered">
+            Don't have an account? <Link to="/createaccount">Create one</Link>
+          </p>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default ResetPassword;
