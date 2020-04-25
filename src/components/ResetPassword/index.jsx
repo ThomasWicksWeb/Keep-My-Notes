6a3ab6db96ebd62541e6c4a3d2a500ce8bcbdd60
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-04-25 13:22:57 -0400

    Refactor(Added error message on failed form submit)

--- src/components/ResetPassword/index.jsx
+++ src/components/ResetPassword/index.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import 'firebase'
-import {auth} from '../../firebase'
+import 'firebase';
+import { auth } from '../../firebase';
+
+import ErrorMessage from '../Misc/Error';
 
 import 'bulma/css/bulma.css';
 import styles from './ResetPassword.module.scss';
@@ -9,24 +11,39 @@ import styles from './ResetPassword.module.scss';
 const ResetPassword = () => {
   const [emailField, setEmail] = useState('');
 
+  const [error, setError] = useState({
+    IsError: false,
+    ErrorTitle: '',
+    ErrorMessage: '',
+  });
+
   const handleEmail = (e) => {
     setEmail(e.target.value);
   };
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
+    auth
+      .sendPasswordResetEmail(emailField)
+      .then(function () {
+        // Email sent.
+      })
+      .catch(function (error) {
+        setError({
+          IsError: true,
+          ErrorTitle: error.code,
+          ErrorMessage: error.message
+        })
+        CheckForError();
+      });
   };
 
-//   var emailAddress = 'stringemail';
+const CheckForError = () => {
+  if(error.IsError){
+    return <ErrorMessage ErrorTitle={error.ErrorTitle} ErrorBody={error.ErrorMessage} />
+  }
+}
 
-//   auth
-//     .sendPasswordResetEmail(emailAddress)
-//     .then(function () {
-//       // Email sent.
-//     })
-//     .catch(function (error) {
-//       // An error happened.
-//     });
 
   return (
     <section className="section">
@@ -34,6 +51,8 @@ const ResetPassword = () => {
         <h3 className="has-text-weight-bold has-text-centered is-size-3">
           Reset your Password
         </h3>
+
+        {CheckForError()}
 
         <form onSubmit={handleOnSubmit} className={styles.formContainer}>
           <div className="field">
