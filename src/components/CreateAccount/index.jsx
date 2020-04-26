269b197ecd7ec1b6e607086b86f257980f4a7f40
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-04-25 21:31:41 -0400

    Removed ErrorMessage props

--- src/components/CreateAccount/index.jsx
+++ src/components/CreateAccount/index.jsx
@@ -13,7 +13,6 @@ const CreateAccount = () => {
 
   const [error, setError] = useState({
     IsError: false,
-    ErrorTitle: '',
     ErrorMessage: '',
   });
 
@@ -38,7 +37,6 @@ const CreateAccount = () => {
       .catch(function (error) {
         setError({
           IsError: true,
-          ErrorTitle: error.code,
           ErrorMessage: error.message,
         });
         CheckForError();
@@ -47,12 +45,7 @@ const CreateAccount = () => {
 
   const CheckForError = () => {
     if (error.IsError) {
-      return (
-        <ErrorMessage
-          ErrorTitle={error.ErrorTitle}
-          ErrorBody={error.ErrorMessage}
-        />
-      );
+      return <ErrorMessage ErrorBody={error.ErrorMessage} />;
     }
   };
 
