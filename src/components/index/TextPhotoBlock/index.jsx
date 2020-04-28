36a0dbce926b95baff963f1fbb4516f0b2e57804
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-04-27 21:50:34 -0400

    Ran Prettier

--- src/components/index/TextPhotoBlock/index.jsx
+++ src/components/index/TextPhotoBlock/index.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import classnames from 'classnames';
-import Emoji from '../../Misc/Emoji'
+import Emoji from '../../Misc/Emoji';
 import styles from './TextPhotoBlock.module.scss';
 
-const TextPhotoBlock = ({ header, paragraph1, paragraph2, img, emoji, emojiLabel }) => {
+const TextPhotoBlock = ({
+  header,
+  paragraph1,
+  paragraph2,
+  img,
+  emoji,
+  emojiLabel,
+}) => {
   return (
     <section className="section">
-      <article className={classnames("container", styles.MainWrapper)}>
-        <div className={classnames("content", styles.content)}>
-          <h2 className="is-size-2">{header}<Emoji Emoji={emoji} Label={emojiLabel} /></h2>
+      <article className={classnames('container', styles.MainWrapper)}>
+        <div className={classnames('content', styles.content)}>
+          <h2 className="is-size-2">
+            {header} <Emoji Emoji={emoji} Label={emojiLabel} />
+          </h2>
           <p className="is-size-5">{paragraph1}</p>
           <p className="is-size-5">{paragraph2}</p>
         </div>
         <div className={styles.ImageContent}>
-            <img src={img} alt="Home page splash"/>
+          <img src={img} alt="Home page splash" />
         </div>
       </article>
     </section>
