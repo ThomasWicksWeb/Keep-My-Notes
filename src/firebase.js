9eb327645cba65fd68d9fb3ec59cdd777e18fab9
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-04-28 21:47:32 -0400

    Ran prettier

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase'
+import firebase from 'firebase';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyAuu4t_3W5SLjCoQAnvxCbPrJIjL-EYw8w",
-    authDomain: "keepmynotes-f9ade.firebaseapp.com",
-    databaseURL: "https://keepmynotes-f9ade.firebaseio.com",
-    projectId: "keepmynotes-f9ade",
-    storageBucket: "keepmynotes-f9ade.appspot.com",
-    messagingSenderId: "550390801016",
-    appId: "1:550390801016:web:4f55f59dc7d5060da6438d",
-    measurementId: "G-QC7YZL9R8X"
+  apiKey: 'AIzaSyAuu4t_3W5SLjCoQAnvxCbPrJIjL-EYw8w',
+  authDomain: 'keepmynotes-f9ade.firebaseapp.com',
+  databaseURL: 'https://keepmynotes-f9ade.firebaseio.com',
+  projectId: 'keepmynotes-f9ade',
+  storageBucket: 'keepmynotes-f9ade.appspot.com',
+  messagingSenderId: '550390801016',
+  appId: '1:550390801016:web:4f55f59dc7d5060da6438d',
+  measurementId: 'G-QC7YZL9R8X',
 };
 
 // Initialize Firebase
