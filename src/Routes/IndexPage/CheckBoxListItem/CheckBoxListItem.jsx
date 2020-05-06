acec83291208f6b0b4a8547f681667bb5a01fdd2
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-05-06 17:02:53 -0400

    Added PropTypes checking

--- src/Routes/IndexPage/CheckBoxListItem/CheckBoxListItem.jsx
+++ src/Routes/IndexPage/CheckBoxListItem/CheckBoxListItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classnames from 'classnames';
+import PropTypes from 'prop-types'
 import styles from './CheckBoxListItem.module.scss';
 
 const CheckBoxListItem = ({ icon, title, subtitle }) => {
@@ -12,6 +13,12 @@ const CheckBoxListItem = ({ icon, title, subtitle }) => {
       <h4 className={classnames('is-size-6 has-text-grey', styles.iconSubtitle)}>{subtitle}</h4>
     </li>
   );
+};
+
+CheckBoxListItem.propType = {
+  icon: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
 };
 
 export default CheckBoxListItem;
