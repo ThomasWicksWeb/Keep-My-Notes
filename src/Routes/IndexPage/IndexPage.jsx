2b4d62ca6c4bb7e68c86c8e63d5d4c55d23f3cee
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-05-21 18:43:57 -0400

    Refactor: Reworked entire page's style

--- src/Routes/IndexPage/IndexPage.jsx
+++ src/Routes/IndexPage/IndexPage.jsx
@@ -3,17 +3,20 @@ import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import classnames from 'classnames';
 
-import { AuthContext } from '../../contexts/AuthContext'
+import { AuthContext } from '../../contexts/AuthContext';
 
 import { TextPhotoBlock } from './TextPhotoBlock';
 import { TextPhotoBlockReversed } from './TextPhotoBlockReversed';
 import { CheckBoxListItem } from './CheckBoxListItem';
 import Emoji from '../../components/Emoji';
 import styles from './Index.module.scss';
-import BackgroundImage from '../../images/blue-bg-2.png';
+
+// Images
+import BackgroundImage from '../../images/blue-bg-4.png';
+import NotesExampleImage from '../../images/screenshot-notes.png';
+import FileSearchImage from '../../images/file-search.svg';
 
 const IndexPage = () => {
-  
   const { userState } = useContext(AuthContext);
 
   // Buttons in the 'call to action' are dependent on if a user is logged in or not
@@ -42,11 +45,14 @@ const IndexPage = () => {
 
   const backgroundImageURL = {
     backgroundImage: `url(${BackgroundImage})`,
-  }
+  };
 
   return (
     <>
-      <section className={classnames("section", styles.SplashSection)}  style={backgroundImageURL}>
+      <section
+        className={classnames('section', styles.SplashSection)}
+        style={backgroundImageURL}
+      >
         <article className={classnames('container', styles.MainWrapper)}>
           <div className={classnames('content', styles.content)}>
             <h2 className={classnames('is-size-1', styles.ScriptFont)}>
@@ -69,16 +75,17 @@ const IndexPage = () => {
 
       <div className="container">
         <TextPhotoBlock
-          header="Stored in the cloud"
-          emoji="🌥️"
-          emojiLabel="High above in the cloud!"
-          paragraph1="All your data is backed up in the cloud so once you create a note, there's zero worry that you'll lose it"
-          img={'./images/UndrawCloudSync.svg'}
+          header="Note taking, kept super simple"
+          emoji="📝"
+          emojiLabel="Notepad"
+          paragraph1="A super simple notes app with none of the bloat."
+          paragraph2="It's so easy to find over-engineered applications these days, one of the main goals with Keep My Notes was to keep is straight-forward and easy to use."
+          img={NotesExampleImage}
         />
 
         <hr />
 
-        <ul className={classnames("container", styles.ListItemContainer)}>
+        <ul className={classnames('container', styles.ListItemContainer)}>
           <CheckBoxListItem
             icon="fas fa-mobile"
             title="Works on all platforms"
@@ -99,12 +106,12 @@ const IndexPage = () => {
         <hr />
 
         <TextPhotoBlockReversed
-          header="Completely Secured Data"
-          emoji="🔒"
-          emojiLabel="Locked' n' secure!"
-          paragraph1="All of your notes are securely stored on Google's servers, for your eyes only"
-          paragraph2="So not only are your notes safe from hackers, but your notes will reach you in milliseconds with Google's infrastructure"
-          img={'./images/UndrawSecurity.svg'}
+          header="Easily search through all of your notes"
+          emoji=" 🔎"
+          emojiLabel="Searchin' searchin' searchin'!"
+          paragraph1="With our powerful search, you're able to easily find any of your notes in your collection."
+          paragraph2="There's no longer any need to worry about endlessly scrolling through all of your notes to find the one you're looking for - all of that hassle is far behind you."
+          img={FileSearchImage}
         />
       </div>
 
